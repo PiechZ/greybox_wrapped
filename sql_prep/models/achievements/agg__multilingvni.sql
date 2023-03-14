9f19f586a4476fd29fec93cff855c7eabcdb4559
Simon Podhajsky <simon.podhajsky@gmail.com>
2023-03-14 11:40:39 +0100

    Rewrite achievement to comply with API contract

--- sql_prep/models/achievements/agg__multilingvni.sql
+++ sql_prep/models/achievements/agg__multilingvni.sql
@@ -15,16 +15,32 @@ languages_per_debater as (
     group by 1
 ),
 
-final as (
+achievement as (
     select
         clovek_id,
+        pocet_jazyku,
         case 
             when pocet_jazyku > 2 then 'multilingvní'
             when pocet_jazyku > 1 then 'bilingvní'
             else null
-        end as achievement
+        end as achievement_text
     from languages_per_debater
+    where clovek_id is not null
+    and pocet_jazyku > 1
+),
+
+final as (
+    select
+        clovek_id,
+        'achievement_linguist/' || clovek_id as achievement_id,
+        'Lingvista' as achievement_name,
+        'Gratuluji, jsi debatně ' || achievement_text || '!' as achievement_description,
+        json_object('pocet_jazyku', pocet_jazyku) as achievement_data,  -- => e.g. {"pocet_jazyku": 3}
+        pocet_jazyku + 3 as achievement_priority
+    from
+        achievement
+    where
+        achievement_text is not null
 )
 
 select * from final
-where achievement is not null
