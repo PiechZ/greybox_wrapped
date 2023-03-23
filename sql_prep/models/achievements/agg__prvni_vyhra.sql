10dbec4e86d8d4fcb38258dcb0379663426a6056
PiechZ <126514453+PiechZ@users.noreply.github.com>
2023-03-23 19:23:40 +0100

    fix lepsi_nez_prumer and prvni_vyhra

--- sql_prep/models/achievements/agg__prvni_vyhra.sql
+++ sql_prep/models/achievements/agg__prvni_vyhra.sql
@@ -1,35 +1,37 @@
-WITH vyhra AS (
-    SELECT 
+WITH base AS (
+    SELECT * FROM {{ ref('base__debater_debata') }}
+),
 
+newbies AS (
+    SELECT * from base
+    RIGHT JOIN {{ ref('base__newbies') }} using (clovek_id)
+),
+
+vyhry AS (
+    SELECT
+        clovek_id,
+        school_year,
+        debate_date,
+    FROM newbies
+    WHERE is_winner = true
+    ORDER BY debate_date
 ),
 
 achievement AS (
-    SELECT
-        clovek_id,
-        rocnik,
-        debate_date,
-        CASE
-            WHEN COUNT(rocnik) = 1 THEN 'První ročník' 
-            ELSE null
-        END AS condition_first,
-        
-        CASE
-            WHEN vyhra THEN 'První výhra!' 
-            ELSE null
-        END AS achievement_text,
+    SELECT DISTINCT ON (clovek_id) *
+    FROM vyhry
 ),
 
 final AS ( 
     SELECT    
         clovek_id,
-        rocnik,
+        school_year,
         debate_date,
         'První výhra' AS achievement_name,
-        achievement_text || 'Letos se ti povedlo dosáhnout první výhry, a to' || debate_date || '!' AS achievement_description,
-        'achievement_prvni_vyhra/' || clovek_id || '/' || rocnik AS achievement_id,
+        'Letos se ti povedlo dosáhnout první výhry, a to' || debate_date || '!' AS achievement_description,
+        'achievement_prvni_vyhra/' || clovek_id || '/' || school_year AS achievement_id,
         'binary' AS achievement_type,
     FROM achievement
-    WHERE condition_first IS NOT null 
 )
 
 SELECT * FROM final
