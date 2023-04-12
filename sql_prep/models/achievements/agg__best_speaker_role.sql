78e3d95e3fd722ba2765d4f198012d31de1a9228
Simon Podhajsky <simon.podhajsky@gmail.com>
2023-04-12 23:16:50 +0200

    Run through sqlfluff

--- sql_prep/models/achievements/agg__best_speaker_role.sql
+++ sql_prep/models/achievements/agg__best_speaker_role.sql
@@ -1,60 +1,62 @@
-WITH speaker_points_sum AS (
-  SELECT
+with speaker_points_sum as (
+  select
     clovek_id,
     school_year,
     role,
-    avg(kidy) AS total_points,
+    avg(kidy) as total_points,
     count(*) as samples
-  FROM
+  from
     {{ ref('base__debater_debata') }}
-  WHERE kidy is not null
-  GROUP BY
+  where kidy is not null
+  group by
     clovek_id,
     school_year,
     role
 ),
 
-best_speaker AS (
-  SELECT
+best_speaker as (
+  select
     school_year,
     role,
     clovek_id,
     total_points,
     samples,
-    RANK() OVER (PARTITION BY school_year, role ORDER BY total_points DESC) AS speaker_rank
-  FROM
+    rank() over (
+      partition by school_year, role order by total_points desc
+    ) as speaker_rank
+  from
     speaker_points_sum
-  WHERE samples >= 5
+  where samples >= 5
 ),
 
 achievement as (
-    SELECT
-        school_year,
-        role,
-        clovek_id,
-        total_points,
-        speaker_rank,
-        samples
-    FROM
-        best_speaker
-    WHERE
-        speaker_rank = 1
-    ORDER BY
-        school_year,
-        role
+  select
+    school_year,
+    role,
+    clovek_id,
+    total_points,
+    speaker_rank,
+    samples
+  from
+    best_speaker
+  where
+    speaker_rank = 1
+  order by
+    school_year,
+    role
 ),
 
 final as (
-    select
-        clovek_id,
-        school_year,
-        {{ make_achievement_id('best_speaker_role') }},
-        'Nejlepší mluvčí roku v roli ' || upper(role) || '!' as achievement_name,
-        round(total_points, 2) || ' je skvělý výkon :)' as achievement_description,
-        7 as achievement_priority,
-        'binary' as achievement_type,
-        json_object('role', role, 'total_points', total_points) as achievement_data
-    from achievement
+  select
+    clovek_id,
+    school_year,
+    {{ make_achievement_id('best_speaker_role') }},
+    'Nejlepší mluvčí roku v roli ' || upper(role) || '!' as achievement_name,
+    round(total_points, 2) || ' je skvělý výkon :)' as achievement_description,
+    7 as achievement_priority,
+    'binary' as achievement_type,
+    json_object('role', role, 'total_points', total_points) as achievement_data
+  from achievement
 )
 
 select * from final
