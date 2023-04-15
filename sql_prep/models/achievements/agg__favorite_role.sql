ef2b5f177e3cd20bfa2e9d019a999924a3910198
Simon Podhajsky <simon.podhajsky@gmail.com>
2023-04-15 19:33:39 +0200

    Break position count ties using average points

--- sql_prep/models/achievements/agg__favorite_role.sql
+++ sql_prep/models/achievements/agg__favorite_role.sql
@@ -30,7 +30,7 @@ debater_position_rank as (
     school_year,
     role,
     position_count,
-    RANK() over (
+    DENSE_RANK() over (
       partition by clovek_id, school_year order by position_count desc
     ) as position_rank
   from
@@ -50,6 +50,8 @@ debater_position_with_league_count as (
   inner join
     league_position_count
     using (school_year, role)
+  where
+    debater_position_rank.position_rank = 1
 ),
 
 debater_avg_points as (
@@ -73,7 +75,6 @@ achievement as (
     debater_position_with_league_count.school_year,
     debater_position_with_league_count.role,
     debater_position_with_league_count.position_count,
-    debater_position_with_league_count.position_rank,
     debater_position_with_league_count.league_position_count,
     debater_avg_points.avg_points,
     (
@@ -86,26 +87,48 @@ achievement as (
   inner join
     debater_avg_points using (clovek_id, school_year, role)
   where
-    debater_position_with_league_count.position_rank = 1
-    and debater_position_with_league_count.position_count >= 5
+    debater_position_with_league_count.position_count >= 5
+),
+
+deduplicated_achievements as (
+  select
+    clovek_id,
+    school_year,
+    role,
+    position_count,
+    league_position_count,
+    avg_points,
+    individual_frequency_percentage,
+    ROW_NUMBER() over (partition by clovek_id, school_year order by avg_points desc) as rank_by_avg_points
+  from
+    achievement
+),
+
+highest_avg_point_achievements as (
+  select
+    *
+  from
+    deduplicated_achievements
+  where
+    rank_by_avg_points = 1
 ),
 
 final as (
   select
     clovek_id,
     school_year,
-    {{ make_achievement_id('favorite_role', 'clovek_id || role') }},
+    {{ make_achievement_id('favorite_role') }},
     'Tvá oblíbená role je...' as achievement_name,
     '...' || upper(role) || '! Dosáhl/a jsi na ní průměrně ' || round(avg_points, 2) || ' bodů.' as achievement_description,
     json_object(
       'role', role,
-      'avg_points', avg_points, 
+      'avg_points', avg_points,
       'position_count', position_count,
       'individual_frequency_percentage', individual_frequency_percentage
     ) as achievement_data,
     'binary' as achievement_type,
     6 as achievement_priority
-  from achievement
+  from highest_avg_point_achievements
 )
 
 select * from final
