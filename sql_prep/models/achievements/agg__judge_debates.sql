27ba9b0de2a88f83180048088142725905e30e5a
Simon Podhajsky <simon.podhajsky@gmail.com>
2023-04-15 08:38:02 +0200

    Fix judge achievement ranges

--- sql_prep/models/achievements/agg__judge_debates.sql
+++ sql_prep/models/achievements/agg__judge_debates.sql
@@ -15,14 +15,14 @@ summary_characteristics as (
     select
         debate_stats.*,
         case
-            when hour_count >= 3 then 'příjemný film (a pak o něm napsat recenzi)'
-            when hour_count >= 6 then 'pořádný spánek (a pak napsat argumentativní VŠ esej)'
-            when hour_count >= 9 then 'půltucet epizod Soudkyně Barbary (a pak napsat dvakrát tolik mstivých ČSFD recenzí)'
-            when hour_count >= 12 then 'trilogii Problém tří těles (a pak se o něm dva dny písemně hádat na Redditu)'
-            when hour_count >= 18 then 'všechny Harry Pottery (a pak se týden hádat na Facebooku o transfobii JK Rowling)'
-            when hour_count >= 30 then 'týdenní šichtu (a pak o ní napsat díl do Hrdinů kapitalistické práce)'
-            when hour_count >= 45 then 'týdenní sekci El Camino Santiago (a pak o ní napsat 30 lifestylových článků do Vlasty)'
-            when hour_count >= 60 then 'všechny seriály a filmy Aarona Sorkina (a pak napsat zbylých 51 Federalist Papers)'
+            when hour_count <= 3 then 'příjemný film (a pak o něm napsat recenzi)'
+            when hour_count <= 6 then 'pořádný spánek (a pak napsat argumentativní VŠ esej)'
+            when hour_count <= 9 then 'půltucet epizod Soudkyně Barbary (a pak napsat dvakrát tolik mstivých ČSFD recenzí)'
+            when hour_count <= 12 then 'trilogii Problém tří těles (a pak se o něm dva dny písemně hádat na Redditu)'
+            when hour_count <= 18 then 'všechny Harry Pottery (a pak se týden hádat na Facebooku o transfobii JK Rowling)'
+            when hour_count <= 30 then 'půltýdenní sekci El Camino Santiago (a pak o ní napsat 30 lifestylových článků do Vlasty)'
+            when hour_count <= 45 then 'týdenní šichtu (a pak o ní napsat díl do Hrdinů kapitalistické práce)'
+            when hour_count > 45 then 'všechny seriály a filmy Aarona Sorkina (a pak napsat zbylých 51 Federalist Papers)'
 
         end as activity
     from debate_stats
@@ -38,9 +38,9 @@ final AS (
         json_object('debate_count', debate_count, 'hour_count', hour_count) as achievement_data,
         4 as achievement_priority,
         'numeric' as achievement_type
-        
     FROM 
         summary_characteristics
+    WHERE activity is not null
 )
 
 SELECT * FROM final
